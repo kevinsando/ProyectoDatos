9184c2a4505214fd00df14cb02f55fd04de7a016
Kevin <kevin_sm20@hotmail.com>
2019-09-28 22:54:15 -0600

    .

--- main.cpp
+++ main.cpp
@@ -8,201 +8,172 @@
 using namespace std;
 
 
-//int max = 2568;
+int max = 2568;
 
-//void display(int arr[]) {
-//    int ctr = 0;
-//    for (int i = 0; i < max; i++) {
-//        if (!ctr && arr[i])
-//            ctr = 1;
-//        if (ctr)
-//            std::cout << arr[i];
-//    }
-//}
-//
-//int fibonacci(int arr[], int n) {
-//    if (n == 0 || n == 1)
-//        return n;
-//    else
-//        return fibonacci(arr, n - 2) + fibonacci(arr, n - 1);
-//}
-//
-//void factorial(int arr[], int n) {
-//    if (!n) return;
-//    int carry = 0; //Valor de acarreo
-//    for (int i = max - 1; i >= 0; --i) {//Del tamaño menos 1 del arreglo(Por aquello de tener que acarrear al final) hasta 0
-//        arr[i] = (arr[i] * n) + carry; //El valor en la posición i del arreglo sería ese dígito, multiplicado por n + el carreo
-//        carry = arr[i] / 10;
-//        arr[i] %= 10;
-//    }
-//    factorial(arr, n - 1);
-//}
+void display(int arr[]) {
+    int ctr = 0;
+    for (int i = 0; i < max; i++) {
+        if (!ctr && arr[i])
+            ctr = 1;
+        if (ctr)
+            std::cout << arr[i];
+    }
+}
+
+int fibonacci(int arr[], int n) {
+    if (n == 0 || n == 1)
+        return n;
+    else
+        return fibonacci(arr, n - 2) + fibonacci(arr, n - 1);
+}
+
+void factorial(int arr[], int n) {
+    if (!n) return;
+    int carry = 0; //Valor de acarreo
+    for (int i = max - 1; i >= 0; --i) {//Del tamaño menos 1 del arreglo(Por aquello de tener que acarrear al final) hasta 0
+        arr[i] = (arr[i] * n) + carry; //El valor en la posición i del arreglo sería ese dígito, multiplicado por n + el carreo
+        carry = arr[i] / 10;
+        arr[i] %= 10;
+    }
+    factorial(arr, n - 1);
+}
 
 int main() {
-    //    int *arr = new int[max];
-    //    
-    //    std::memset(arr,0,max*sizeof(int));//Incializo la matríz con 0s
-    //    arr[max-1] = 1;
-    //    int num;
-    //    std::cout<<"Digite un numero: ";
-    //    std::cin>>num;
-    //    std::cout<<"el factorial de "<<num<<" es :\n";
-    //    factorial(arr,num);
-    //    display(arr);
-    //    delete[] arr;
-    //    std::cin.get();
-    //    std::cin.get();
-    
-    //*** test Multiplicacion ***
-    
-//    arreglo *a1 = new arreglo();
-//    arreglo *a2 = new arreglo();
-//    lista *l1 = new lista();
-//    lista *l2 = new lista();
-//    //integer* num = new integer(1232132);
-//    
-//
-//   
-//   a1->agregarFinal(555555555);
-//   a1->agregarFinal(777777777);
-//    
-////    a1->agregarFinal(5555);
-////    a1->agregarFinal(4444);
-//    
-//   
-//  // a2->agregarFinal(22922291);
-//    
-//    a2->agregarFinal(222222222);
-//    a2->agregarFinal(667666666);
-////    a2->agregarFinal(1111);
-//    //                    
-//    l1->agregar(a1);
-//    l2->agregar(a2);
-//    lista *l3 = l1->operator - (l2);
-//    //              
-//    cout << " " << l1->toString() << endl;
-//    cout << " " << l2->toString() << endl;
-//    cout << "-__________________" << endl;
-//    cout << " " << l3->toString() << endl;
-//
- /*   integer* num = new integer(l3);
-    cout<<num->toString();
-    */
+    int *arr = new int[max];
 
-    /*TEST METODOS FORMULAS*/
-//arreglo* a1 = new arreglo();
-//arreglo* a2 = new arreglo();
-//lista* l1 = new lista();
-//lista* l2 = new lista();
-//a1->agregar(123456789);
-//a1->agregar(987654321);
-//a2->agregar(123456789);
-//a2->agregar(987654321);
-//l1->agregar(a1);
-//l2->agregar(a2);
-//cout << l1->toString() << endl;
-//cout << l2->toString() << endl;
-//if (l1->operator!=(l2))
-//{
-//	cout << "Son distintos";
-//}
-//else
-//{
-//	cout<< "son iguales";
-//}
-//
-//arreglo* a20 =new arreglo();
-//arreglo* a30 = new arreglo();
-//lista *l10 = new lista();
-//lista *l20 = new lista();
-//
-//
-//a20->agregarFinal(55555);
-//a30->agregarFinal(44444);
-//
-//a20->agregarFinal(11111);
-//a30->agregarFinal(1111);
-//
-//
-//l10->agregar(a20);
-//l20->agregar(a30);
-//
-//
-//
-//cout<<endl;
-//cout<<l10->toString()<<" + "<<l20->toString()<<endl;
-//
-//l10 = l10->operator +=(l20);
-//
-//cout<<"L10 SOBRECARGA: "<<l10->toString()<<endl;
+    std::memset(arr, 0, max * sizeof (int)); //Incializo la matríz con 0s
+    arr[max - 1] = 1;
+    int num;
+    std::cout << "Digite un numero: ";
+    std::cin>>num;
+    std::cout << "el factorial de " << num << " es :\n";
+    factorial(arr, num);
+    display(arr);
+    delete[] arr;
+    std::cin.get();
+    std::cin.get();
+
+
+
+
+    arreglo* a111 = new arreglo();
+    arreglo* a222 = new arreglo();
+    lista* l111 = new lista();
+    lista* l222 = new lista();
+    a111->agregar(123456789);
+    a111->agregar(987654321);
+    a222->agregar(123456789);
+    a222->agregar(987654321);
+    l111->agregar(a111);
+    l222->agregar(a222);
+    cout << l111->toString() << endl;
+    cout << l222->toString() << endl;
+    if (l111->operator!=(l222)) {
+        cout << "Son distintos";
+    } else {
+        cout << "son iguales";
+    }
+
+    arreglo* a20 = new arreglo();
+    arreglo* a30 = new arreglo();
+    lista *l10 = new lista();
+    lista *l20 = new lista();
+
+
+    a20->agregarFinal(55555);
+    a30->agregarFinal(44444);
+
+    a20->agregarFinal(11111);
+    a30->agregarFinal(1111);
+
+
+    l10->agregar(a20);
+    l20->agregar(a30);
+
+
+
+    cout << endl;
+    cout << l10->toString() << " + " << l20->toString() << endl;
+
+    l10 = l10->operator+=(l20);
+
+    cout << "L10 SOBRECARGA: " << l10->toString() << endl;
 
 
 
 
 
 
-//unsigned t0, t1;
-//
-//t0 = clock();
-//int n = 1000;
-//lista* l1 = lista::fibonacci(n);
-//	cout<<"FIBONACCI DE "<<n<<endl<<l1->toString();
-//	delete l1;
-//	// Code to execute
-//	t1 = clock();
-//	double time = (double(t1 - t0) / CLOCKS_PER_SEC);
-//	cout << "Execution Time: " << time << endl;
-	//lista* l1 = new lista();
-//integer* num = new integer(l1);
-//num->parse("43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875");
-//cout << num->toString(); 
-//	cin.get();
-    
-    
-    
-    
+    unsigned t0, t1;
+
+    t0 = clock();
+    int n = 1000;
+    lista* l1 = lista::fibonacci(n);
+    cout << "FIBONACCI DE " << n << endl << l1->toString();
+    delete l1;
+    // Code to execute
+    t1 = clock();
+    double time = (double(t1 - t0) / CLOCKS_PER_SEC);
+    cout << "Execution Time: " << time << endl;
+    lista* l9 = new lista();
+    integer* num = new integer(l9);
+    num->parse("43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875");
+    cout << num->toString();
+    cin.get();
+
+
+
+
     lista *l1 = new lista();
     lista *l2 = new lista();
     lista* l3 = new lista();
     arreglo *a1 = new arreglo();
     arreglo *a2 = new arreglo();
-    arreglo* a3= new arreglo();
-   a1->agregarFinal(999999999);
-   a1->agregarFinal(999999999);
-   a1->agregarFinal(999999999);
-   a1->agregarFinal(999999999);
-   a1->agregarFinal(999999999);
-   a1->agregarFinal(999999999);
-   a1->agregarFinal(999999999);
-   a1->agregarFinal(999999999);
-   a1->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a2->agregarFinal(999999999);
-   a3->agregarFinal(999999999);
+    arreglo* a3 = new arreglo();
+    a1->agregarFinal(999999999);
+    a1->agregarFinal(999999999);
+    a1->agregarFinal(999999999);
+    a1->agregarFinal(999999999);
+    a1->agregarFinal(999999999);
+    a1->agregarFinal(999999999);
+    a1->agregarFinal(999999999);
+    a1->agregarFinal(999999999);
+    a1->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a2->agregarFinal(999999999);
+    a3->agregarFinal(999999999);
     l1->agregar(a1);
     l2->agregar(a2);
-    
-    cout<<"Numero 1: "<<l1->toString()<<endl;
-    cout<<"Numero 2: "<<l2->toString()<<endl;
-    cout<<"Son iguales?: "<<l1->operator ==(l2)<<" ,Diferentes?: "<<l1->operator !=(l2)<<endl;
-    cout<<"N1 mayor?: "<<l1->operator >(l2)<<" ,N2 menor?: "<<l1->operator <(l2)<<endl;
-    cout<<"N3=N1+N2"<<endl;
-    l3=l1->operator +(l2);
-    cout<<l1->toString()<<" + "<<l2->toString()<<endl;
-    cout<<"N3: "<<l3->toString()<<endl;
-    cout<<"N3 mayor que N1?: "<<l3->operator >(l1)<<", es N1 menor que N3?: "<<l1->operator <(l3)<<endl;
-    cout<<"se eliminan N1, N2 y N3"<<endl;
+
+    cout << "Numero 1: " << l1->toString() << endl;
+    cout << "Numero 2: " << l2->toString() << endl;
+    cout << "Son iguales?: " << l1->operator==(l2) << " ,Diferentes?: " << l1->operator!=(l2) << endl;
+    cout << "N1 mayor?: " << l1->operator>(l2) << " ,N2 menor?: " << l1->operator<(l2) << endl;
+
+    cout << "N3=N1+N2" << endl;
+    l3 = l1->operator+(l2);
+    cout << l1->toString() << " + " << l2->toString() << endl;
+    cout << "N3: " << l3->toString() << endl;
+
+    cout << "N4=N1-N2" << endl;
+    lista* l4 = l1->operator-(l2);
+    cout << l1->toString() << " - " << l2->toString() << endl;
+    cout << "N4: " << l3->toString() << endl;
+
+
+    cout << "N3 mayor que N1?: " << l3->operator>(l1) << ", es N1 menor que N3?: " << l1->operator<(l3) << endl;
+    cout << "se eliminan N1, N2 y N3" << endl;
     delete l3;
     delete l2;
     delete l1;
-    cout<<"Se les asignan nuevos numeros"<<endl;
+    cout << "Se les asignan nuevos numeros" << endl;
     l1 = new lista();
     l2 = new lista();
     a1 = new arreglo();
@@ -219,34 +190,14 @@ int main() {
     a2->agregarFinal(982434258);
     l1->agregar(a1);
     l2->agregar(a2);
-    cout<<"N1: "<<l1->toString()<<endl;
-    cout<<"N2: "<<l2->toString()<<endl;
-    l3=l1->operator *(l2);
-    cout<<"N3= N1*N2: "<<l3->toString()<<endl;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    cout << "N1: " << l1->toString() << endl;
+    cout << "N2: " << l2->toString() << endl;
+    l3 = l1->operator*(l2);
+    cout << "N3= N1*N2: " << l3->toString() << endl;
+
+    cout << "N1: " << l1->toString() << endl;
+    cout << "N2: " << l2->toString() << endl;
+
 
     return 0;
 }
